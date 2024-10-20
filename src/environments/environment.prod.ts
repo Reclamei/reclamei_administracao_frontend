613e4006a610ed648d4db4853759142ccf4344be
livia <liviaaurich2@gmail.com>
2024-10-20 20:35:18 -0300

    Editar reclamações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,16 +5,18 @@ declare var RECLAMEI_FIREBASE_STORAGE_BUCKET: string;
 declare var RECLAMEI_FIREBASE_MESSAGING_SENDER_ID: string;
 declare var RECLAMEI_FIREBASE_APP_ID: string;
 declare var RECLAMEI_FIREBASE_MEASUREMENT_ID: string;
+declare var GATEWAY_ENDPOINT: string;
 
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: RECLAMEI_FIREBASE_API_KEY,
-    authDomain: RECLAMEI_FIREBASE_AUTH_DOMAIN,
-    projectId: RECLAMEI_FIREBASE_PROJECT_ID,
-    storageBucket: RECLAMEI_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: RECLAMEI_FIREBASE_MESSAGING_SENDER_ID,
-    appId: RECLAMEI_FIREBASE_APP_ID,
-    measurementId: RECLAMEI_FIREBASE_MEASUREMENT_ID
-  }
+    production: true,
+    apiEndpoint: GATEWAY_ENDPOINT,
+    firebase: {
+        apiKey: RECLAMEI_FIREBASE_API_KEY,
+        authDomain: RECLAMEI_FIREBASE_AUTH_DOMAIN,
+        projectId: RECLAMEI_FIREBASE_PROJECT_ID,
+        storageBucket: RECLAMEI_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: RECLAMEI_FIREBASE_MESSAGING_SENDER_ID,
+        appId: RECLAMEI_FIREBASE_APP_ID,
+        measurementId: RECLAMEI_FIREBASE_MEASUREMENT_ID
+    }
 };
